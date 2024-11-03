166d1764fb171167f38ec509c5f9af79cabd5e3c
shnfi <mrplom7@gmail.com>
2024-11-03 14:13:30 +0330

    modification - clear the screen with the builtin function

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 # include "get_output.h"
 # include "builtin_commands/exit_c.h"
 # include "builtin_commands/help_c.h"
+# include "builtin_commands/clear_c.h"
 
 # define USERNAME get_output("whoami")
 # define MAX_COMMAND_LEN 500
@@ -19,7 +20,7 @@
 typedef struct
 {
 	char data[MAX_COMMAND_LEN];
-	int len;
+	int *len;
 } command_s;
 
 int main()
@@ -30,11 +31,13 @@ int main()
 	scrollok(stdscr, true);
 	keypad(stdscr, true);
 
-	int line = 0;
+	int *line = malloc(500);
+	*line = 0;
+
 	bool *EXIT = malloc(sizeof(bool));
 	*EXIT = false;
 
-	command_s command = { "", 0 };
+	command_s command = { "", malloc(MAX_COMMAND_LEN) };
 
 	do
 	{
@@ -42,8 +45,8 @@ int main()
   		struct tm tm = *localtime(&t);
   		//printw("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); // a line for finding the date and time format specifiers (delete me later!)
 
-		mvprintw(line, 0, "[%02d:%02d:%02d]-[%s]~> ", tm.tm_hour, tm.tm_min, tm.tm_sec, USERNAME);
-		mvprintw(line, sizeof(USERNAME) + 18, "%s", command.data);
+		mvprintw(*line, 0, "[%02d:%02d:%02d]-[%s]~> ", tm.tm_hour, tm.tm_min, tm.tm_sec, USERNAME);
+		mvprintw(*line, sizeof(USERNAME) + 18, "%s", command.data);
 		
 		int ch = getch();
 
@@ -53,7 +56,7 @@ int main()
 				struct winsize max;
 				ioctl(0, TIOCGWINSZ , &max);
 
-				if (line >= max.ws_row - 5)
+				if (*line >= max.ws_row - 5)
 				{
 					return 0; // i will add a clear screen or text overflow haldler in this position later!
 				}
@@ -71,11 +74,7 @@ int main()
 				}
 				else if (strcmp(command.data, "clear") == 0)
 				{
-					clear();
-					//erase();
-					line = 0;
-					strcpy(command.data, "");
-					command.len = 0;
+					clear_c(line, command.data, command.len);
 					break;
 				}
 				else
@@ -104,12 +103,12 @@ int main()
 					free(returning_output);
 				}
 
-				if (command.len != 0) line += 2;
+				if (*command.len != 0) *line += 2;
 
-				line += *additional_line + 1;
+				*line += *additional_line + 1;
 				*additional_line = 0;
 				strcpy(command.data, "");
-				command.len = 0;
+				*command.len = 0;
 
 				free(additional_line);
 				memset(command.data, 0, sizeof(command.data));
@@ -117,19 +116,19 @@ int main()
 				break;
 
 			case 263 : // 'backspace' key
-				if (command.len > 0)
+				if (*command.len > 0)
 				{
-					mvdelch(line, sizeof(USERNAME) + 18 + command.len - 1);
-					command.data[command.len-1] = 0;
-					command.len -= 1;
+					mvdelch(*line, sizeof(USERNAME) + 18 + *command.len - 1);
+					command.data[*command.len-1] = 0;
+					*command.len -= 1;
 				}
 
 				break;
 
 			case IS_CTRL_PRESSED('c') : // 'ctrl' key + 'c' key
-				line += 1;
+				*line += 1;
 				strcpy(command.data, "");
-				command.len = 0;
+				*command.len = 0;
 				break;
 
 			case IS_CTRL_PRESSED('q') : // 'ctrl' key + 'q' key
@@ -154,8 +153,8 @@ int main()
 
 			default : 
 				//mvprintw(1, 100, "%d", ch); // a line for finding the code of keys (delete me later!)
-				command.data[command.len] = (char) ch;
-				command.len += 1;
+				command.data[*command.len] = (char) ch;
+				*command.len += 1;
 				break;
 		}
 		
