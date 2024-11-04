85676e5ad322185f869292a6f129f68e509718f8
shnfi <mrplom7@gmail.com>
2024-11-04 18:31:51 +0330

    first entry - wrote a function to get the called function

---
+++ called_command_finder.h
@@ -0,0 +1,14 @@
+# include <string.h>
+
+char *called_command_finder(char str[])
+{
+   static char output[15];
+
+   for (int i = 0; i < strlen(str); i++)
+   {
+      if (str[i] != ' ') output[strlen(output)] = str[i];
+      else break;
+   }
+
+   return output;
+}
