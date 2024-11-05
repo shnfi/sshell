3765ac6992295041316300e97d0e6088cb839087
shnfi <mrplom7@gmail.com>
2024-11-05 18:12:15 +0330

    bug fixed - the program was only able to call called_command_finder function once

--- called_command_finder.h
+++ called_command_finder.h
@@ -4,6 +4,8 @@ char *called_command_finder(char str[])
 {
    static char output[15];
 
+   memset(output, 0, sizeof(output));
+
    for (int i = 0; i < strlen(str); i++)
    {
       if (str[i] != ' ') output[strlen(output)] = str[i];
