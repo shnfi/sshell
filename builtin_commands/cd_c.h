a9a9cf9e98ffe2516c45b4d63f7a0c3549a0c2e6
shnfi <mrplom7@gmail.com>
2024-11-05 18:21:17 +0330

    first entry - builtin function to change directory

---
+++ builtin_commands/cd_c.h
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <string.h>
+
+void cd_c(char str[])
+{
+   int ws_index;
+   static char new_dir[30];
+
+   memset(new_dir, 0, sizeof(new_dir));
+
+   for (int i = 0; i < strlen(str); i++)
+   {
+      if (str[i] == ' ')
+      {
+         ws_index = i+1;
+         break;
+      }
+   }
+
+   for (int i = ws_index; i < strlen(str); i++) new_dir[strlen(new_dir)] = str[i];
+
+   chdir(new_dir);
+}
