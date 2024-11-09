c295040351cefbbdd7789ca910b2493e43947824
shnfi <mrplom7@gmail.com>
2024-11-09 18:11:13 +0330

    first entry - function for getting the current working directory name, not an the full address

---
+++ current_dir_name.h
@@ -0,0 +1,26 @@
+#include <string.h>
+
+#ifndef BUFFER
+   #define BUFFER 30
+#endif
+
+char *current_dir_name(char cwd[])
+{
+   static char dir_n[BUFFER];
+   static char r_dir_n[BUFFER];
+
+   memset(dir_n, 0, sizeof(dir_n));
+
+   for (int i = strlen(cwd) - 1; i >= 0; i--)
+   {
+      if (cwd[i] != '/')
+      {
+         dir_n[strlen(dir_n)] = cwd[i];
+      }
+      else break;
+   }
+
+   for (int i = strlen(dir_n) - 1; i >= 0; i--) r_dir_n[strlen(r_dir_n)] = dir_n[i];
+
+   return r_dir_n;
+}
