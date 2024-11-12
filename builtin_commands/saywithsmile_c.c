26ae42d729144bbe75996465246f570243b1c7b7
shnfi <mrplom7@gmail.com>
2024-11-12 20:48:12 +0330

    modification - changed the style of the 'saywithsmile' command

--- builtin_commands/saywithsmile_c.c
+++ builtin_commands/saywithsmile_c.c
@@ -5,7 +5,7 @@ void saywithsmile_c(char str[], int *al)
 {
    unsigned int index_of_whitespace;
 
-   for (int i = 0; i < strlen(str); i++)
+   for (int i = 0; i <= strlen(str); i++)
    {
       if (str[i] == ' ') 
       {
@@ -14,8 +14,26 @@ void saywithsmile_c(char str[], int *al)
       }
    }
 
-   printw("\n\n:) -> ");
-   for (int i = index_of_whitespace; i < strlen(str); i++) printw("%c", str[i]); 
+   printw("\n\n");
 
-   *al += 1;
+   for (int i = 0; i < (strlen(str) - index_of_whitespace) + 1; i++) printw("-");
+
+   printw("\n");
+
+   for (int i = index_of_whitespace; i < strlen(str); i++)  printw("%c", str[i]);
+
+   printw("\n");
+   
+   for (int i = 0; i < (strlen(str) - index_of_whitespace) + 1; i++) printw("-");
+   
+   printw("\n");
+
+   for (int i = 0; i < (strlen(str) - index_of_whitespace) / 3; i++)
+   {
+      printw(" ");
+
+      if (i == (strlen(str) - index_of_whitespace) / 3 - 1) printw("¯\\_(*o*)_/¯");
+   }
+
+   *al += 4;
 }
