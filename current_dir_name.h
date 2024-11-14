0e22f4cc961e20705cf63951c88d0be3b5f76999
shnfi <mrplom7@gmail.com>
2024-11-14 13:56:27 +0330

    bug fixed - fixed the buffer problem of the current directory name's length and its pointers in this function

--- current_dir_name.h
+++ current_dir_name.h
@@ -1,12 +1,13 @@
 #include <string.h>
 
-#define CDN_BUFFER 30
+#define CDN_BUFFER 50
 
 char *current_dir_name(char cwd[])
 {
-   char dir_n[CDN_BUFFER];
-   static char r_dir_n[CDN_BUFFER];
+   char *dir_n = malloc(CDN_BUFFER);
+   char *r_dir_n = malloc(CDN_BUFFER);
 
+   memset(dir_n, 0, sizeof(dir_n));
    memset(r_dir_n, 0, sizeof(r_dir_n));
 
    for (int i = strlen(cwd) - 1; i >= 0; i--)
