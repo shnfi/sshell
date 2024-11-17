f81312c2ee9886725b82044aec5383fc60a4dc84
shnfi <mrplom7@gmail.com>
2024-11-17 17:56:57 +0330

    first entry - builtin function to list directories

---
+++ builtin_commands/ls_c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <dirent.h>
+
+void ls_c(char cwd[], int *al)
+{
+   DIR *path = opendir(cwd);
+   struct dirent *d = readdir(path);
+
+   int occupied_lines;
+
+   printw("\n\n");
+
+   while (d != NULL)
+   {
+      if (d->d_name != "." || d->d_name != "..")
+      {
+         printw("[D]-%s\n", d->d_name);
+         d = readdir(path);
+         occupied_lines++;
+      }
+      else continue;
+   }
+
+   *al += occupied_lines;
+}
