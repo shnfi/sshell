823f219e51467ce24e5d408a60eb1e0c46101ff0
shnfi <mrplom7@gmail.com>
2024-11-23 18:57:40 +0330

    modification - changed the style of the 'uptime' output

--- builtin_commands/uptime_c.c
+++ builtin_commands/uptime_c.c
@@ -6,7 +6,7 @@ void uptime_c(long ut, int *al)
    int min = (ut % 3600) / 60;
 
    printw("\n\n");
-   printw("%d:%d", hour, min);
+   printw("Started about %02d:%02d", hour, min);
 
    *al += 1;
 }
