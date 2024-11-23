f59e5a83c58b50332958300786a25e77608b8cc1
shnfi <mrplom7@gmail.com>
2024-11-23 19:05:46 +0330

    feature added - adding the 'started from' section to the output

--- builtin_commands/uptime_c.c
+++ builtin_commands/uptime_c.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void uptime_c(long ut, int *al)
+void uptime_c(long ut, int SS, int SM, int SH, int *al)
 {
    int hour = ut / 3600;
    int min = (ut % 3600) / 60;
 
    printw("\n\n");
-   printw("Started about %02d:%02d", hour, min);
+   printw("Started from %02d:%02d:%02d - Started about %02d:%02d hour", SS, SM, SH, hour, min);
 
    *al += 1;
 }
