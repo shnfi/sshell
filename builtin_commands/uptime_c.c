ee2b46c54ba21fb9643749d94460933f605aebc6
shnfi <mrplom7@gmail.com>
2024-11-25 18:52:54 +0330

    feature added - new style in 'uptime'  output if it was less that a hour

--- builtin_commands/uptime_c.c
+++ builtin_commands/uptime_c.c
@@ -6,7 +6,15 @@ void uptime_c(long ut, int *al)
    int min = (ut % 3600) / 60;
 
    printw("\n\n");
-   printw("Started about %02d:%02d hour", hour, min);
+   
+   if (hour == 0)
+   {
+      printw("Started about %02d minutes", min);
+   }
+   else
+   {
+      printw("Started about %02d:%02d hours", hour, min);
+   }
 
    *al += 1;
 }
