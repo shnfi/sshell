be526e52bd6205fdc084aa3503093224f0bf03ad
shnfi <mrplom7@gmail.com>
2024-11-25 18:29:27 +0330

    removed feature - removed the 'started from' part of the 'uptime' output because of a bug that resets the 'SS', 'SM' and 'SH' variable's value (this feature will be back soon)

--- builtin_commands/uptime_c.c
+++ builtin_commands/uptime_c.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void uptime_c(long ut, int SS, int SM, int SH, int *al)
+void uptime_c(long ut, int *al)
 {
    int hour = ut / 3600;
    int min = (ut % 3600) / 60;
 
    printw("\n\n");
-   printw("Started from %02d:%02d:%02d - Started about %02d:%02d hour", SS, SM, SH, hour, min);
+   printw("Started about %02d:%02d hour", hour, min);
 
    *al += 1;
 }
