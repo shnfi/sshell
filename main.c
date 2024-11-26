a274c4fab8b040ae3e3256ce614a92b8fe2218ce
shnfi <mrplom7@gmail.com>
2024-11-26 19:38:40 +0330

    feature added - deleting files and directories using the builtin 'rm' command

--- main.c
+++ main.c
@@ -31,6 +31,7 @@
 #include "builtin_commands/uptime_c.c"
 #include "builtin_commands/touch_c.c"
 #include "builtin_commands/mkdir_c.c"
+#include "builtin_commands/rm_c.c"
 
 #define USERNAME get_output("whoami")
 #define MAX_COMMAND_LEN 500
@@ -147,6 +148,7 @@ int main()
 				else if (strcmp(command.data, "uptime") == 0) uptime_c(info.uptime, additional_line);
 				else if (strcmp(called_command_finder(command.data), "touch") == 0) touch_c(getcwd(NULL, 100), command.data);
 				else if (strcmp(called_command_finder(command.data), "mkdir") == 0) mkdir_c(getcwd(NULL, 100), command.data);
+				else if (strcmp(called_command_finder(command.data), "rm") == 0) rm_c(getcwd(NULL, 100), command.data);
 				else
 				{
 					/*
