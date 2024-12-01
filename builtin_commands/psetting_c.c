5fd7a10563747093e7b2f1c06f4b54173b1c0a87
shnfi <mrplom7@gmail.com>
2024-12-01 17:36:41 +0330

    feature added - you are allow to choose between round brackets and square brackets for your prompt customization

--- builtin_commands/psetting_c.c
+++ builtin_commands/psetting_c.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void psetting_c(char str[], char **c_or_d)
+void psetting_c(char str[], char **c_or_d, char **r_or_s)
 {
    /*
     * argument parsing
@@ -27,10 +27,18 @@ void psetting_c(char str[], char **c_or_d)
    
    if (strcmp(arg, "-c") == 0)
    {
-      *c_or_d = "c";
+      *c_or_d = "clock";
    }
    else if (strcmp(arg, "-d") == 0)
    {
-      *c_or_d = "d";
+      *c_or_d = "date";
+   }
+   else if (strcmp(arg, "-r") == 0)
+   {
+      *r_or_s = "round";
+   }
+   else if (strcmp(arg, "-s") == 0)
+   {
+      *r_or_s = "square";
    }
 }
