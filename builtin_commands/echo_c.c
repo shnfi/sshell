338bb58c0bceb65e7d76b49947a7227e31303997
shnfi <mrplom7@gmail.com>
2024-12-07 23:31:31 +0330

    first entry - builtin function for echoing different values

---
+++ builtin_commands/echo_c.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void echo_c(char str[], int *al)
+{
+   printw("\n\n");
+
+   if (strstr(str, "$") != NULL)
+   {
+      /*
+       * echoing the local variable of the system
+       */
+      
+      char *x = malloc(12);
+
+      for (int i = 6; i < strlen(str); i++)
+      {
+         x[strlen(x)] = str[i];
+      }
+
+      char *l_var = getenv(x);
+
+      if (l_var != NULL)
+      {
+         printw("%s", l_var);
+      }
+      else
+      {
+         printw(" * This local variable not existed!");
+      }
+
+      free(x);
+
+      *al += 1;
+   }
+   else
+   {
+      /*
+       * echoing the given value
+       */
+      
+      for (int i = 5; i < strlen(str); i++)
+      {
+         printw("%c", str[i]);
+      }
+      
+      *al += 1;
+   }
+}
