ec65c87b4bd57471e78cfc62511080e2e533cf82
shnfi <mrplom7@gmail.com>
2024-12-10 19:13:41 +0330

    feature added - now you can see the help of each command separately just by writing the name of it after the help word

--- builtin_commands/help_c.c
+++ builtin_commands/help_c.c
@@ -1,11 +1,35 @@
-void help_c(char username[], int *al)
-{
-	printw("\n\nWELCOME TO SSHELL, DEAR %s\n", username);
-	printw("sshell is stands for simple shell, maybe .\n\n");
-	printw("there is some of unique commands of sshell :\n\n");
-	printw("help -> shows the help screen .\n");
-	printw("saywithsmile [word] -> prints your word with smiling face .\n");
-	printw("psetting (-c/-d/-s/-r) -> choose between clock and date to show in prompt (-c = clock, -d = date, -s = square, -r = round) .\n");
+#include <string.h>
 
-	*al += 8;
+void help_c(char str[], char username[], int *al)
+{
+	printw("\n\n");
+
+	char *c = malloc(15);
+
+	for (int i = 5; i < strlen(str) && str[i] != ' '; i++)
+	{
+		c[strlen(c)] = str[i];
+	}
+
+	if (strlen(c) == 0)
+	{
+		printw(" * Please use the 'help help' command to find the 'help' command's syntax.");
+		*al += 1;
+	}
+
+	if (strcmp(c, "help") == 0)
+	{
+		printw("Usage: help [command]\nShows the help screen of any command.\n");
+		*al += 2;
+	}
+	else if (strcmp(c, "saywithsmile") == 0)
+	{
+		printw("Usage: saywithsmile [sentence]\nPrints your sentence with a smiling face.\n");
+		*al += 2;
+	}
+	else if (strcmp(c, "psetting") == 0)
+	{
+		printw("Usage: psetting [setting]\nCustomizes your prompt.\n\n   -c         shows the clock on prompt\n   -d         shows the date on prompt\n   -s         uses square brackets for prompt\n   -r         uses round brackets for prompt\n");
+		*al += 7;
+	}
 }
