e39eb4477a8f579f33fbfdc059c1c49fd20ae245
shnfi <mrplom7@gmail.com>
2024-12-11 16:56:45 +0330

    feature added - added a better system to set the type and theme for prompt

--- builtin_commands/psetting_c.c
+++ builtin_commands/psetting_c.c
@@ -3,37 +3,43 @@
 
 #include "headers/check_for_arg.h"
 
-void psetting_c(char str[], char **c_or_d, char **r_or_s)
+void psetting_c(char str[], char **c_or_d, char **r_or_s, char **prompt_type)
 {  
    if (check_for_arg(str, "c") == 1)
    {
       *c_or_d = "clock";
+      *prompt_type = "-1";
    }
    
    if (check_for_arg(str, "d") == 1)
    {
       *c_or_d = "date";
+      *prompt_type = "-1";
    }
    
    if (check_for_arg(str, "r") == 1)
    {
       *r_or_s = "round";
+      *prompt_type = "-1";
    }
    
    if (check_for_arg(str, "s") == 1)
    {
       *r_or_s = "square";
+      *prompt_type = "-1";
    }
 
    if (check_for_arg(str, "1") == 1)
    {
       *c_or_d = "-1";
       *r_or_s = "-1";
+      *prompt_type = "1";
    }
 
    if (check_for_arg(str, "2") == 1)
    {
-      *c_or_d = "-2";
-      *r_or_s = "-2";
+      *c_or_d = "-1";
+      *r_or_s = "-1";
+      *prompt_type = "2";
    }
 }
