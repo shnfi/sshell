f946c47bb14a61a333871a9abb9eac617cbf7102
shnfi <mrplom7@gmail.com>
2024-12-13 13:48:46 +0330

    first entry - builtin function for seaching in the firefox

---
+++ builtin_commands/search_c.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+void search_c(char str[], int *line, int *command_len)
+{
+   char *command = malloc(207);
+   char *val = malloc(200);
+
+   for (int i = 7; i < strlen(str); i++)
+   {
+      if (str[i] != ' ')
+      {
+         val[strlen(val)] = str[i];
+      }
+      else 
+      {
+         val[strlen(val)] = '+';
+      }
+   }
+
+   sprintf(command, "firefox https://www.google.com/search?q=%s -safe-mode", val);
+
+   system(command);
+
+   clear_c(line, str, command_len);
+}
