0ae1b2c86154cf0a6b3ab52621ce78e551a55e08
shnfi <mrplom7@gmail.com>
2024-12-15 19:53:00 +0330

    modification - changed the directory name identification method to use less pointers in the code (low memory usage)

--- headers/current_dir_name.h
+++ headers/current_dir_name.h
@@ -1,25 +1,29 @@
 #include <string.h>
 
-#define CDN_BUFFER 50
+#define CDN_BUFFER 25
 
 char *current_dir_name(char cwd[])
 {
    char *dir_n = malloc(CDN_BUFFER);
-   char *r_dir_n = malloc(CDN_BUFFER);
+   int index_of_s;
 
    memset(dir_n, 0, sizeof(dir_n));
-   memset(r_dir_n, 0, sizeof(r_dir_n));
 
-   for (int i = strlen(cwd) - 1; i >= 0; i--)
+   for (int i = strlen(cwd); i >= 0; i--)
    {
-      if (cwd[i] != '/')
+      if (cwd[i] == '/')
       {
-         dir_n[strlen(dir_n)] = cwd[i];
+         index_of_s = i;
+         break;
       }
-      else break;
    }
 
-   for (int i = strlen(dir_n) - 1; i >= 0; i--) r_dir_n[strlen(r_dir_n)] = dir_n[i];
+   for (int i = 0; i < (strlen(cwd) - index_of_s - 1); i++)
+   {
+      dir_n[strlen(dir_n)] = cwd[(index_of_s + 1) + i];
+   }
 
-   return r_dir_n;
+   return dir_n;
+
+   free(dir_n);
 }
