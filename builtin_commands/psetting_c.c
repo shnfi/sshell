548f649a684781d398130bf864a16df9cca3aefa
shnfi <mrplom7@gmail.com>
2024-12-21 18:51:20 +0330

    feature added - added an option controlling system that check if the switches that user used are valid or not

--- builtin_commands/psetting_c.c
+++ builtin_commands/psetting_c.c
@@ -3,8 +3,23 @@
 
 #include "headers/check_for_arg.h"
 
-void psetting_c(char str[], char **c_or_d, char **r_or_s, char **prompt_type)
-{  
+char *option_validation(char str[], char *valid_options[], int valid_options_size);
+
+void psetting_c(char str[], char **c_or_d, char **r_or_s, char **prompt_type, int *al)
+{
+   char *valid_options[6] = { "c", "d", "r", "s", "1", "2" };
+
+   char *validate_output = option_validation(str, valid_options, sizeof(valid_options) / sizeof(valid_options[0]));
+
+   printw("\n\n");
+
+   if (strcmp(validate_output, "okay") != 0)
+   {
+      printw(" * Invalid options used: %s\n", validate_output);
+      *al += 1;
+      return;
+   }
+
    if (check_for_arg(str, "c") == 1)
    {
       *c_or_d = "clock";
