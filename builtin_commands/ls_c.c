3f4b7e43bd19a812ebafa6ec9b17a7194d9c74fa
shnfi <mrplom7@gmail.com>
2024-12-21 17:58:16 +0330

    modification - moved the responsibility of the options validation to another separate function

--- builtin_commands/ls_c.c
+++ builtin_commands/ls_c.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "headers/option_validation.h"
+
 int check_for_arg(char str[], char arg[]);
 
 void ls_c(char str[], char cwd[], int *al)
@@ -11,66 +13,16 @@ void ls_c(char str[], char cwd[], int *al)
    struct dirent *d = readdir(path);
 
    char *valid_options[5] = { "l", "t", "r", "h", "a" };
-   char *args_string = malloc(8);
 
-   memset(args_string, 0, sizeof(args_string));
-
-   int occupied_lines;
-   int index_of_dashes[5];
-   int x = 0;
+   char *validate_output = option_validation(str, valid_options, sizeof(valid_options) / sizeof(valid_options[0]));
 
    printw("\n\n");
 
-   for (int i = 0; i < strlen(str); i++)
+   if (strcmp(validate_output, "okay") != 0)
    {
-      if (str[i] == '-')
-      {
-         index_of_dashes[x] = i;
-         x++;
-      }
-      else continue;
-   }
-
-   for (int i = 0; i < x; i++)
-   {
-      for (int j = index_of_dashes[i]; j < strlen(str); j++)
-      {
-         if (str[j] != ' ')
-         {
-            args_string[strlen(args_string)] = str[j];
-         }
-      }
-   }
-
-   /*
-    * checking for invalid options
-    */
-
-   bool er = false;
-
-   for (int i = 1; i < strlen(args_string); i++)
-   {
-      for (int j = 0; j < sizeof(valid_options) / sizeof(valid_options[0]); j++)
-      {
-         char *ASI_PTR = malloc(10);
-
-         sprintf(ASI_PTR, "%c", args_string[i]);
-
-         if (strcmp(ASI_PTR, valid_options[j]) == 0)
-         {
-            er = false;
-            break;
-         }
-         else
-            er = true;
-      }
-
-      if (er)
-      {
-         printw(" * Invalid options used: %c\n", args_string[i]);
-         *al += 1;
-         return;
-      }
+      printw(" * Invalid options used: %s\n", validate_output);
+      *al += 1;
+      return;
    }
 
    while (d != NULL)
