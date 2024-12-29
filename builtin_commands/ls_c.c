caa97f18bceaa8197cb36ec946655a14df52b469
shnfi <mrplom7@gmail.com>
2024-12-29 16:40:03 +0330

    modification - clean code on the listing part of the code

--- builtin_commands/ls_c.c
+++ builtin_commands/ls_c.c
@@ -76,82 +76,47 @@ void ls_c(char str[], char cwd[], int *al)
     * at the end the main directory listing task..
     */
 
+   DIR *path;
+
    if (strlen(dir_name) == 0)
-   {
-      DIR *path = opendir(cwd);
-      struct dirent *d = readdir(path);
-
-      while (d != NULL)
-      {
-         char symbol;
-
-         /*
-          * checking the type of the file in the directory and setting the symbol for listing it
-          */
-
-         if (d->d_type == DT_DIR) symbol = 'D';
-         else if (d->d_type == DT_REG) symbol = 'F';
-         else if (d->d_type == DT_UNKNOWN) symbol = '?';
-         else symbol = 'X';
-
-         /*
-          * preparing the size of each file 
-          */
-
-         if (d->d_name[0] != '.')
-         {
-            printw("[ %c ] %s\n", symbol, d->d_name);
-            *al += 1;
-         }
-
-         d = readdir(path);
-
-         if ((char *) d == NULL) 
-         {
-            free(path);
-            free(d);
-
-            return;
-         }
-      }
-   }
+      path = opendir(cwd);
    else
+      path = opendir(dir_name);
+
+   struct dirent *d = readdir(path);
+
+   while (d != NULL)
    {
-      DIR *path = opendir(dir_name);
-      struct dirent *d = readdir(path);
+      char symbol;
 
-      while (d != NULL)
+      /*
+       * checking the type of the file in the directory and setting the symbol for listing it
+       */
+
+      if (d->d_type == DT_DIR) symbol = 'D';
+      else if (d->d_type == DT_REG) symbol = 'F';
+      else if (d->d_type == DT_UNKNOWN) symbol = '?';
+      else symbol = 'X';
+
+      /*
+       * preparing the size of each file 
+       */
+
+      if (d->d_name[0] != '.')
       {
-         char symbol;
+         printw("[ %c ] %s\n", symbol, d->d_name);
+         *al += 1;
+      }
 
-         /*
-          * checking the type of the file in the directory and setting the symbol for listing it
-          */
+      d = readdir(path);
 
-         if (d->d_type == DT_DIR) symbol = 'D';
-         else if (d->d_type == DT_REG) symbol = 'F';
-         else if (d->d_type == DT_UNKNOWN) symbol = '?';
-         else symbol = 'X';
+      if ((char *) d == NULL) 
+      {
+         free(path);
+         free(d);
+         free(dir_name);
 
-         /*
-          * preparing the size of each file 
-          */
-
-         if (d->d_name[0] != '.')
-         {
-            printw("[ %c ] %s\n", symbol, d->d_name);
-            *al += 1;
-         }
-
-         d = readdir(path);
-
-         if ((char *) d == NULL) 
-         {
-            free(path);
-            free(d);
-
-            return;
-         }
+         return;
       }
    }
 }
