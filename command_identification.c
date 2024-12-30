23812764293edf29c8c43457e8786d94b47220cb
shnfi <mrplom7@gmail.com>
2024-12-30 18:22:13 +0330

    bug fixed - this bug resets color pair after an error or alert

--- command_identification.c
+++ command_identification.c
@@ -3,22 +3,22 @@ void command_identification(
    char *prompt_type[], int *using_color_index
    )
 {
-   if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "help") == 0) help_c(command_data, USERNAME, additional_line);
+   if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "help") == 0) help_c(command_data, USERNAME, additional_line, using_color_index);
    else if (strcmp(trim_extra_spaces(command_data), "exit") == 0) exit_c(EXIT);
    else if (strcmp(trim_extra_spaces(command_data), "clear") == 0) clear_c(line, command_data, command_len);
    else if (strcmp(trim_extra_spaces(command_data), "cd") == 0) { chdir("/home/"); chdir(USERNAME); } /* navigating to the home directory shortcut */
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "saywithsmile") == 0) saywithsmile_c(command_data, additional_line);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "cd") == 0) cd_c(command_data);
-   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "ls") == 0) ls_c(command_data, getcwd(NULL, 100), additional_line);
+   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "ls") == 0) ls_c(command_data, getcwd(NULL, 100), additional_line, using_color_index);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "psetting") == 0) psetting_c(command_data, clock_or_date, round_or_square, prompt_type, additional_line, using_color_index);
    else if (strcmp(trim_extra_spaces(command_data), "pwd") == 0) pwd_c(getcwd(NULL, 100), additional_line);
    else if (strcmp(trim_extra_spaces(command_data), "uptime") == 0) uptime_c(info.uptime, additional_line);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "touch") == 0) touch_c(getcwd(NULL, 100), command_data);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "mkdir") == 0) mkdir_c(getcwd(NULL, 100), command_data);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "rm") == 0) rm_c(getcwd(NULL, 100), command_data);
-   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "echo") == 0) echo_c(command_data, additional_line);
+   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "echo") == 0) echo_c(command_data, additional_line, using_color_index);
    else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "search") == 0) search_c(command_data, line, command_len);
-   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "sizeof") == 0) sizeof_c(command_data, additional_line);
+   else if (strcmp(called_command_finder(trim_extra_spaces(command_data)), "sizeof") == 0) sizeof_c(command_data, additional_line, using_color_index);
    else
    {
       /*
@@ -36,11 +36,10 @@ void command_identification(
          attron(COLOR_PAIR(3));
 
          printw(" [ERROR] Error while opening the pipe.");
-         *al += 1;
+         *additional_line += 1;
 
          attroff(COLOR_PAIR(3));
-
-         return "ERROR";
+         attron(COLOR_PAIR(*using_color_index));
       }
       else
       {
